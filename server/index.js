915fbb15ac6c1e551ab76c2d18125be4a501e39c
Kishan269565 <kishangupta269565@gmail.com>
2026-05-31 18:18:33 +0530

    fix CORS for production

--- server/index.js
+++ server/index.js
@@ -7,7 +7,15 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://e-commerce-platform-kishanprojects.vercel.app',
+  ],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 // Routes
